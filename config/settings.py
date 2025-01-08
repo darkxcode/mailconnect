18aa8caacef244929d089edd279fe092060687c9
The-Young-Programer <abduramonibrahim@gmail.com>
2025-01-08 23:11:14 +0100

    Update deployment configuration

--- config/settings.py
+++ config/settings.py
@@ -1,15 +1,40 @@
+import os
 from utils.logging import SafeFileHandler
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
     'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
         'file': {
             '()': 'utils.logging.SafeFileHandler',
             'filename': os.path.join(BASE_DIR, 'logs', 'django.log'),
             'formatter': 'verbose',
+            'mode': 'a',
         },
-        // other handlers...
     },
-    // rest of logging config...
+    'root': {
+        'handlers': ['console', 'file'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+            'propagate': False,
+        },
+    }
 } 
